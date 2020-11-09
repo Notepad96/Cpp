0f965794efb3c066c3281d593c6003608940bc73
Notepad96 <notepad1029@gmail.com>
2020-11-09 10:08:26 +0900

    201109

---
+++ max_element.cpp
@@ -0,0 +1,34 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+    vector<int> v;
+    v.push_back(46);
+    v.push_back(68);
+    v.push_back(184);
+    v.push_back(4);
+    v.push_back(17);
+    v.push_back(53);
+    
+    cout << "현재 vector : ";
+    for (int i : v) cout << i << " ";
+    cout << "\n==============================\n";
+
+    int max = *max_element(v.begin(), v.end());
+    cout << "가장 큰 수 : " << max << "\n";
+    
+    int max_index = max_element(v.begin(), v.end()) - v.begin();
+    cout << "가장 큰 수의 인덱스 : " << max_index;
+    cout << "\n==============================\n";
+
+
+    int min = *min_element(v.begin(), v.end());
+    cout << "가장 작은 수 : " << min << "\n";
+
+    int min_index = min_element(v.begin(), v.end()) - v.begin();
+    cout << "가장 작은 수의 인덱스 : " << min_index << "\n\n";
+
+    return 0;
+}
