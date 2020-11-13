f539eb5717593e8f0e389b28b898e7a7cec95bae
Notepad96 <notepad1029@gmail.com>
2020-11-13 10:29:39 +0900

    201113

---
+++ copy.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> v = { 10, 20, 30, 40, 50 };
+    for (int i : v) cout << i << " ";
+    cout << "\n=============================\n";
+
+    // vector<int> v2;  // error 발생
+    vector<int> v2(10,0);
+    
+    copy(v.begin(), v.end(), v2.begin()+5);
+    //copy_n(v.begin(), 5, v2.begin() + 5);
+    copy_if(v.begin(), v.end(), v2.begin(), [](int i) {return i > 30; });
+    
+    // v2.assign(v.begin(),v.end()); // 10 20 30 40 50 새로 초기화
+
+    for (int i : v2) cout << i << " ";
+    cout << "\n=============================\n";
+    
+
+    v2.insert(v2.end(), v.begin(), v.end());
+    for (int i : v2) cout << i << " ";
+    cout << "\n=============================\n";
+
+    return 0;
+}
---
+++ swap.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a = 10, b = 1000;
+    cout << "a = " <<  a << ", b = " << b << "\n";
+    swap(a, b);
+    cout << "a = " << a << ", b = " << b << "\n";
+    
+    cout << "\n=============================\n";
+    vector<int> v = { 10, 20, 30, 40, 50 };
+    cout << "vector1 : ";
+    for (int i : v) cout << i << " ";
+    
+    vector<int> v2(5,0);
+    cout << "\nvector2 : ";
+    for (int i : v2) cout << i << " ";
+    cout << "\n=============================\n";
+    
+    swap_ranges(v.begin(), v.begin() + 3, v2.begin());
+
+    cout << "vector1 : ";
+    for (int i : v) cout << i << " ";
+
+    cout << "\nvector2 : ";
+    for (int i : v2) cout << i << " ";
+    cout << "\n=============================\n";
+
+    return 0;
+}
