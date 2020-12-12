e73d3a83f7f18274a50f4716f2301545b946b4dd
Notepad96 <notepad1029@gmail.com>
2020-12-12 23:02:21 +0900

    201212

---
+++ binary_to_num.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int binary_to_num(string s, int num = 2) {
+	return stoi(s, nullptr, num);
+}
+
+int main() {
+	string s = "10001", s2 = "11112", s3 = "333";
+	cout << s << "(2진수) : " << binary_to_num(s) << "\n";
+	cout << s2 << "(3진수) : " << binary_to_num(s2, 3) << "\n";
+	cout << s3 << "(4진수) : " << binary_to_num(s3, 4) << "\n";
+
+    return 0;
+}
